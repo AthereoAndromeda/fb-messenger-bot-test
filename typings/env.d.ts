cf4426ca8c1954a8f6c68f79c0a7c6d63138626c
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-31 23:18:12 +0800

    Added VERIFY_TOKEN

--- typings/env.d.ts
+++ typings/env.d.ts
@@ -3,6 +3,7 @@ declare global {
         interface ProcessEnv {
             PORT?: string;
             NODE_ENV?: "development" | "production";
+            VERIFY_TOKEN?: string;
         }
     }
 }
