9c08f4e11eae082cab68a2f2ea300182b31efd7d
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-31 22:45:00 +0800

    Added registerRoutes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import { FastifyInstance } from "fastify";
 
+function registerRoutes(app: FastifyInstance) {
+    app.get("/", (req, res) => {
+        res.status(200).send("test");
+    });
+}
+
 export async function buildServer(
     app: FastifyInstance
 ): Promise<FastifyInstance> {
+    registerRoutes(app);
+
     return app;
 }
