fc56a297d9a16dc8ecf97ffbedf267bf200fa8fe
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-08-01 01:36:11 +0800

    Added webhook route

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import { FastifyInstance } from "fastify";
+import webhook from "./routes/webhook";
 
-function registerRoutes(app: FastifyInstance) {
+async function registerRoutes(app: FastifyInstance) {
     app.get("/", (req, res) => {
         res.status(200).send("test");
+    });
+
+    app.register(webhook.route, {
+        prefix: webhook.path,
     });
 }
 
 export async function buildServer(
     app: FastifyInstance
 ): Promise<FastifyInstance> {
-    registerRoutes(app);
+    await registerRoutes(app);
 
     return app;
 }
